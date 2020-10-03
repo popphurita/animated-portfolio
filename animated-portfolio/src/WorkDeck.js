c54922cf36f0276e96cec9db4d91ed5693b5a9eb
Phurita <ps922@bath.ac.uk>
2020-10-03 14:40:48 +0100

    Added AOS for fade in transition

--- animated-portfolio/src/WorkDeck.js
+++ animated-portfolio/src/WorkDeck.js
@@ -5,6 +5,10 @@ import Row from 'react-bootstrap/Row';
 import CardColumns from 'react-bootstrap/CardColumns';
 import Card from 'react-bootstrap/Card';
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import './custom-aos.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function WorkDeck() {
@@ -27,13 +31,19 @@ function WorkDeck() {
   },
   [])
 
+  React.useEffect(() => {
+    AOS.init({
+      duration : 2000
+    })
+  },[])
+
   return (
       <Row className="workdeck-container">
         <CardColumns>
-          <Card className="work-card">
+          <Card className="work-card" data-aos='fade-up-highest'>
             <a href="/">
               <Card.Body className={getRandomColorClassName()}>
-                <Card.Title><h3>Card title</h3></Card.Title>
+                <Card.Title><h3>Card title 1</h3></Card.Title>
                 <Card.Text>
                   This is a wider card with supporting text below as a natural lead-in to
                   additional content. This card has even longer content than the first to
@@ -45,10 +55,10 @@ function WorkDeck() {
               </Card.Body>
             </a>
           </Card>
-          <Card className="work-card">
+          <Card className="work-card" data-aos='fade-up-highest'>
             <a href="/">
               <Card.Body className={getRandomColorClassName()}>
-                <Card.Title><h3>Card title</h3></Card.Title>
+                <Card.Title><h3>Card title 2</h3></Card.Title>
                 <Card.Text>
                   This is a wider card with supporting text below as a natural lead-in to
                   additional content. This card has even longer content than the first to
@@ -62,10 +72,10 @@ function WorkDeck() {
               </Card.Body>
             </a>
           </Card>
-          <Card className="work-card">
+          <Card className="work-card" data-aos='fade-up-highest'>
             <a href="/">
               <Card.Body className={getRandomColorClassName()}>
-                <Card.Title><h3>Card title</h3></Card.Title>
+                <Card.Title><h3>Card title 3</h3></Card.Title>
                 <Card.Text>
                   This is a wider card with supporting text below as a natural lead-in to
                   additional content. This card has even longer content than the first to
@@ -79,10 +89,10 @@ function WorkDeck() {
               </Card.Body>
             </a>
           </Card>
-          <Card className="work-card">
+          <Card className="work-card" data-aos='fade-up-medium'>
             <a href="/">
               <Card.Body className={getRandomColorClassName()}>
-                <Card.Title><h3>Card title</h3></Card.Title>
+                <Card.Title><h3>Card title 4</h3></Card.Title>
                 <Card.Text>
                   This is a wider card with supporting text below as a natural lead-in to
                   additional content. This card has even longer content than the first to
@@ -94,10 +104,10 @@ function WorkDeck() {
               </Card.Body>
             </a>
           </Card>
-          <Card className="work-card">
+          <Card className="work-card" data-aos='fade-up-medium'>
             <a href="/">
               <Card.Body className={getRandomColorClassName()}>
-                <Card.Title><h3>Card title</h3></Card.Title>
+                <Card.Title><h3>Card title 5</h3></Card.Title>
                 <Card.Text>
                   This is a wider card with supporting text below as a natural lead-in to
                   additional content. This card has even longer content than the first to
@@ -111,14 +121,42 @@ function WorkDeck() {
               </Card.Body>
             </a>
           </Card>
-          <Card className="work-card">
+          <Card className="work-card" data-aos='fade-up-medium'>
             <a href="/">
               <Card.Body className={getRandomColorClassName()}>
-                <Card.Title><h3>Card title</h3></Card.Title>
+                <Card.Title><h3>Card title 6</h3></Card.Title>
                 <Card.Text>
                   This is a wider card with supporting text below as a natural lead-in to
                   additional content. This card has even longer content than the first to
-                  show that equal height action. This is a wider card with supporting text below as a natural lead-in to
+                  show that equal height action.
+                </Card.Text>
+                <Card.Text>
+                  <small className="text-muted">Read 1 minute</small>
+                </Card.Text>
+              </Card.Body>
+            </a>
+          </Card>
+          <Card className="work-card" data-aos='fade-up'>
+            <a href="/">
+              <Card.Body className={getRandomColorClassName()}>
+                <Card.Title><h3>Card title 7</h3></Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This card has even longer content than the first to
+                  show that equal height action.
+                </Card.Text>
+                <Card.Text>
+                  <small className="text-muted">Read 1 minute</small>
+                </Card.Text>
+              </Card.Body>
+            </a>
+          </Card>
+          <Card className="work-card" data-aos='fade-up'>
+            <a href="/">
+              <Card.Body className={getRandomColorClassName()}>
+                <Card.Title><h3>Card title 8</h3></Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
                   additional content. This card has even longer content than the first to
                   show that equal height action.
                 </Card.Text>
